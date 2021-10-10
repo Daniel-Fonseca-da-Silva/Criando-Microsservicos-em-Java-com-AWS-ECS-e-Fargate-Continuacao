89ee14f8c329d3467eb72066502aa78f5ad2e3ae
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-10 00:58:47 -0300

    Criada Vpc, adicionada dependência ec2, e listada vpc no terminal com cdk list e feito deploy com cdk deploy vpc

---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,28 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+
+public class VpcStack extends Stack {
+	
+    private Vpc vpc;
+
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        vpc = Vpc.Builder.create(this, "Vpc01")
+                .maxAzs(3)
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+	
+}
